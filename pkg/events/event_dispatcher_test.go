a4a3f129321e0e0b91c568b3312fb304e5ffda41
Danilo Y Kodavara <danilo.kodavara@hotmail.com>
2023-09-22 21:32:09 -0300

    feat: add setPayload.

--- pkg/events/event_dispatcher_test.go
+++ pkg/events/event_dispatcher_test.go
@@ -14,6 +14,10 @@ type TestEvent struct {
 	Payload interface{}
 }
 
+func (e *TestEvent) SetPayload(payload interface{}) {
+	e.Payload = payload
+}
+
 func (e *TestEvent) GetName() string {
 	return e.Name
 }
