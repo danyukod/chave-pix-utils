4e89b9d148cb0426dddeef64067a13c2a8e08fe9
Danilo Y Kodavara <danilo.kodavara@hotmail.com>
2024-01-14 19:22:07 -0300

    feat: create implementation methods

--- pkg/uow/uow.go
+++ pkg/uow/uow.go
@@ -3,6 +3,7 @@ package uow
 import (
 	"context"
 	"database/sql"
+	"fmt"
 )
 
 type RepositoryFactory func(tx *sql.Tx) interface{}
@@ -16,6 +17,13 @@ type UowInterface interface {
 	UnRegister(name string)
 }
 
+type Uow struct {
+	ctx          context.Context
+	Db           *sql.DB
+	Tx           *sql.Tx
+	repositories map[string]RepositoryFactory
+}
+
 func NewUow(ctx context.Context, db *sql.DB) *Uow {
 	return &Uow{
 		ctx:          ctx,
@@ -24,9 +32,37 @@ func NewUow(ctx context.Context, db *sql.DB) *Uow {
 	}
 }
 
-type Uow struct {
-	ctx          context.Context
-	Db           *sql.DB
-	Tx           *sql.Tx
-	repositories map[string]RepositoryFactory
+func (u *Uow) Register(name string, fc RepositoryFactory) {
+	u.repositories[name] = fc
+}
+
+func (u *Uow) GetRepository(ctx context.Context, name string) (interface{}, error) {
+	// Implement the logic to get a repository by its name
+	// For now, we'll just return the repository and nil error
+	if repo, ok := u.repositories[name]; ok {
+		return repo(u.Tx), nil
+	}
+	return nil, fmt.Errorf("repository not found")
+}
+
+func (u *Uow) Do(ctx context.Context, fn func(uow *Uow) error) error {
+	// Implement the logic to execute a function with the Uow as a parameter
+	// For now, we'll just call the function and return its result
+	return fn(u)
+}
+
+func (u *Uow) CommitOrRollback() error {
+	// Implement the logic to commit or rollback the transaction
+	// For now, we'll just return a nil error
+	return nil
+}
+
+func (u *Uow) Rollback() error {
+	// Implement the logic to rollback the transaction
+	// For now, we'll just return a nil error
+	return nil
+}
+
+func (u *Uow) UnRegister(name string) {
+	delete(u.repositories, name)
 }
